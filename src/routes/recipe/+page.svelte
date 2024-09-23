ca5bd906c880eff789afde0ada0bebada09b6f99
Reece Bennett <reece.bennett1@gmail.com>
2024-09-23 17:12:29 +0100

    Add search field to recipe list page

--- src/routes/recipe/+page.svelte
+++ src/routes/recipe/+page.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
   import type { PageData } from './$types';
+  import { filterListByKey } from '$lib/fuzzy';
+
   export let data: PageData;
+
+  let search = '';
+
+  $: recipes = search ? filterListByKey(data.recipes, 'name', search) : data.recipes;
 </script>
 
 <main class="container">
@@ -15,12 +21,20 @@
       <a href="/recipe/new">Create recipe</a>
     </p>
 
-    <ul>
-      {#each data.recipes as { id, name }}
-        <li>
-          <a href="/recipe/{id}">{name}</a>
-        </li>
-      {/each}
-    </ul>
+    <div role="search">
+      <input type="search" placeholder="Search" bind:value={search} />
+    </div>
+
+    {#if recipes.length > 0}
+      <ul>
+        {#each recipes as { id, name }}
+          <li>
+            <a href="/recipe/{id}">{name}</a>
+          </li>
+        {/each}
+      </ul>
+    {:else}
+      <p>No recipes found</p>
+    {/if}
   </section>
 </main>
