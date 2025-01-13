07b58b24560c56df82074577aa784785e7d24083
Reece Bennett <reece.bennett1@gmail.com>
2025-01-13 11:35:31 +0000

    Move layout CSS to separate file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
+  import '../app.css';
   import { pwaInfo } from 'virtual:pwa-info';
-  interface Props {
-    children?: import('svelte').Snippet;
-  }
 
-  let { children }: Props = $props();
+  let { children } = $props();
 
   let webManifestLink = $derived(pwaInfo ? pwaInfo.webManifest.linkTag : '')
 </script>
@@ -13,19 +11,4 @@
  	{@html webManifestLink}
 </svelte:head>
 
-{@render children?.()}
-
-<style>
-  @import 'https://unpkg.com/@picocss/pico@latest/css/pico.min.css';
-
-  :global(body) {
-    padding-top: 2rem;
-  }
-
-  @media (min-width: 768px) {
-    :root {
-      /* Stop pico increasing font size beyond 18px on larger breakpoints */
-      --pico-font-size: 112.5%;
-    }
-  }
-</style>
+{@render children()}
