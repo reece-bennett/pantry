9b936c67949a184278749f95ec1517809f42d851
Reece Bennett <reece.bennett1@gmail.com>
2026-01-06 17:46:50 +0000

    Add copy ingredients button

--- src/routes/list/[id]/+page.svelte
+++ src/routes/list/[id]/+page.svelte
@@ -46,11 +46,72 @@
         return 0;
       })
   );
+
+  let ingredientsTexts = $derived(
+    ingredients.map(({ amount, name, unit }) => `${amount} ${unit !== 'x' ? unit : ''} ${name}`)
+  );
+
+  let copied = $state(false);
+  let error = $state('');
+
+  async function writeIngredientsToClipboard() {
+    try {
+      await navigator.clipboard.writeText(ingredientsTexts.join('\n'));
+    } catch (err) {
+      error = err as string;
+    }
+    copied = true;
+    setTimeout(() => {
+      copied = false;
+    }, 2000);
+  }
 </script>
 
 <div id="root">
   <Header backUrl="/list">
     {#snippet actions()}
+      <button
+        type="button"
+        class="icon"
+        aria-label="Copy ingredients"
+        onclick={writeIngredientsToClipboard}
+        disabled={copied}
+      >
+        {#if copied}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-copy-check-icon lucide-copy-check"
+          >
+            <path d="m12 15 2 2 4-4" />
+            <rect width="14" height="14" x="8" y="8" rx="2" ry="2" />
+            <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" />
+          </svg>
+        {:else}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-copy-icon lucide-copy"
+          >
+            <rect width="14" height="14" x="8" y="8" rx="2" ry="2" />
+            <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" />
+          </svg>
+        {/if}
+      </button>
       <a href="/list/{data.list.id}/edit" class="icon" aria-label="Edit list">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -84,12 +145,8 @@
     <h2>Ingredients</h2>
 
     <ul>
-      {#each ingredients as { amount, name, unit }}
-        <li>
-          {amount}
-          {#if unit !== 'x'}{unit}{/if}
-          {name}
-        </li>
+      {#each ingredientsTexts as ingredient}
+        <li>{ingredient}</li>
       {/each}
     </ul>
   </main>
